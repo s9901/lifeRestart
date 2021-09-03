ff46754c912d938b7204257e88181753b25c9519
s9901 <44739953+s9901@users.noreply.github.com>
2021-09-03 21:37:19 +0800

    Update talent.js

--- src/talent.js
+++ src/talent.js
@@ -60,9 +60,9 @@ class Talent {
                 if(!i && include) return include;
                 const gradeRandom = Math.random();
                 let grade;
-                if(gradeRandom>=0.111) grade = 0;
-                else if(gradeRandom>=0.011) grade = 1;
-                else if(gradeRandom>=0.001) grade = 2;
+                if(gradeRandom>=0.111) grade = 2;
+                else if(gradeRandom>=0.011) grade = 3;
+                else if(gradeRandom>=0.001) grade = 3;
                 else grade = 3;
 
                 while(talentList[grade].length == 0) grade--;
